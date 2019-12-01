d7255a86aaa237ef0ca0ece07a5c8fdfa1fb5704
mic-meier <mic.l.meier@gmail.com>
2019-12-01 12:00:25 +0100

    128 - defaulting video selection

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,21 @@ import youtube from "../apis/youtube";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onSearchTermSubmit("Low Roar Darkest Hour");
+  }
+
   onSearchTermSubmit = async searchTerm => {
     const response = await youtube.get("/search/", {
       params: {
         q: searchTerm
       }
     });
-    this.setState({ videos: response.data.items });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,8 +1,11 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import "./VideoItem.css";
 import React from "react";
 
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
     <div onClick={() => onVideoSelect(video)} className="item video-item">
       <img
         className="ui image"
